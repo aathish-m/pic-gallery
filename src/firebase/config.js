2f5a655b6be7fbd140d11a06103fe6b61a4914e3
aathish-m <aathishm2002@gmail.com>
2022-01-23 20:10:32 +0530

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+//import { getStorage } from "firebase/storage";
+
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyDkus4xY2TtF0JohxGcPyMJhS5J3cDwdY4",
+  authDomain: "pic-gallery-d6831.firebaseapp.com",
+  projectId: "pic-gallery-d6831",
+  storageBucket: "pic-gallery-d6831.appspot.com",
+  messagingSenderId: "1034131519805",
+  appId: "1:1034131519805:web:251dd161b99e8dcf87ad54"
+};
+
+const app = initializeApp(firebaseConfig);
+const projectfirestore = getFirestore(app)
+const projectstorage = getStorage(app);
+
+
+
+
+
+export{ projectstorage, projectfirestore };
